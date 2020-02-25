92224f1d37c8234090955cf397604e2708faec9a
saoramx <saora.mx@gmail.com>
2020-02-25 02:09:08 -0600

    nuevos test cases

--- src/test/java/testsuit/TestSuite.java
+++ src/test/java/testsuit/TestSuite.java
@@ -1,5 +1,8 @@
+package testsuit;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import salesforcemaintest.NewButtonOrLinkTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({NewButtonOrLinkTest.class})
