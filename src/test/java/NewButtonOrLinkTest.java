3d6a4d16d47419d395dafdf8f58eb3f2327db654
saoramx <saora.mx@gmail.com>
2020-02-25 01:56:27 -0600

    nuevos test cases

--- src/test/java/NewButtonOrLinkTest.java
+++ src/test/java/NewButtonOrLinkTest.java
@@ -1,48 +1,107 @@
 import com.DriverFactory;
 import com.DriverType;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import javafx.scene.layout.Priority;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import pages.JsButtonOrLinkPage;
 import pages.SalesforceHomePage;
 import pages.SalesforceLoginPage;
 
-public class NewButtonOrLinkTest {
-    WebDriver driver;
+import java.rmi.Naming;
 
-    @Before
-    public void setUp(){
+public class NewButtonOrLinkTest {
+   static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
         driver = DriverFactory.getDriver(DriverType.CHROME);
         driver.get("https://login.salesforce.com/?locale=eu");
-    }
 
-    @After
-    public void tearDown(){
-
-    }
-
-    @Test
-    public void loginTest(){
         SalesforceLoginPage slogin = new SalesforceLoginPage(driver);
         slogin.typeUsername("glbltest.salesforce@gmail.com");
         slogin.typePassword("test12345");
         slogin.clickLogin();
-
         SalesforceHomePage shome = new SalesforceHomePage(driver);
         shome.switchToClassic();
+    }
+
+    @Before
+    public void antes(){
+        SalesforceHomePage shome = new SalesforceHomePage(driver);
+        shome.clickOnSetUp();
         shome.clickOnCustomizeIcon();
         shome.selCustomizeOption("Campaigns");
         shome.selectObjectOption("Campaign","Buttons, Links, and Actions");
         shome.clickOnNewJsButton();
+    }
 
+    @AfterClass
+    public static void tearDown(){
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test()
+    public void windowAlertTest(){
         JsButtonOrLinkPage jsbtn = new JsButtonOrLinkPage(driver);
-        jsbtn.typeLabel("test3");
+        jsbtn.typeLabel("windowalertsmoketest");
         jsbtn.typeDescription("Description test");
         jsbtn.selectDisplayType("Detail Page Button");
         jsbtn.selectBehavior("Execute JavaScript");
         jsbtn.typeContentEditor("test content editor");
         jsbtn.saveJavaScriptButton();
-
     }
+    @Test
+    public void alertTest(){
+        JsButtonOrLinkPage jsbtn = new JsButtonOrLinkPage(driver);
+        jsbtn.typeLabel("alertsmoketest");
+        jsbtn.typeDescription("Description test");
+        jsbtn.selectDisplayType("Detail Page Button");
+        jsbtn.selectBehavior("Execute JavaScript");
+        jsbtn.typeContentEditor("test content editor");
+        jsbtn.saveJavaScriptButton();
+    }
+
+    @Test
+    public void apexTest(){
+        JsButtonOrLinkPage jsbtn = new JsButtonOrLinkPage(driver);
+        jsbtn.typeLabel("apexsmoketest");
+        jsbtn.typeDescription("Description test");
+        jsbtn.selectDisplayType("Detail Page Button");
+        jsbtn.selectBehavior("Execute JavaScript");
+        jsbtn.typeContentEditor("test content editor");
+        jsbtn.saveJavaScriptButton();
+    }
+    @Test
+    public void ifElseTest(){
+        JsButtonOrLinkPage jsbtn = new JsButtonOrLinkPage(driver);
+        jsbtn.typeLabel("ifelsesmoketest");
+        jsbtn.typeDescription("Description test");
+        jsbtn.selectDisplayType("Detail Page Button");
+        jsbtn.selectBehavior("Execute JavaScript");
+        jsbtn.typeContentEditor("test content editor");
+        jsbtn.saveJavaScriptButton();
+    }
+    @Test
+    public void urlFor(){
+        JsButtonOrLinkPage jsbtn = new JsButtonOrLinkPage(driver);
+        jsbtn.typeLabel("urlforsmoketest");
+        jsbtn.typeDescription("Description test");
+        jsbtn.selectDisplayType("Detail Page Button");
+        jsbtn.selectBehavior("Execute JavaScript");
+        jsbtn.typeContentEditor("test content editor");
+        jsbtn.saveJavaScriptButton();
+    }
+    @Test
+    public void notSupportedObject(){
+        JsButtonOrLinkPage jsbtn = new JsButtonOrLinkPage(driver);
+        jsbtn.typeLabel("notsupportedsmoketest");
+        jsbtn.typeDescription("Description test");
+        jsbtn.selectDisplayType("Detail Page Button");
+        jsbtn.selectBehavior("Execute JavaScript");
+        jsbtn.typeContentEditor("test content editor");
+        jsbtn.saveJavaScriptButton();
+    }
+
 }
