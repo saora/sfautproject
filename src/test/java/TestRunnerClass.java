c37c89fca0ed541b3b229da205dfffb1a5205d01
saoramx <saora.mx@gmail.com>
2020-02-25 01:56:44 -0600

    nuevos test cases

---
+++ src/test/java/TestRunnerClass.java
@@ -0,0 +1,17 @@
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunnerClass {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestSuite.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println();
+        System.out.println("Test Suite execution was successful: "+ result.wasSuccessful());
+    }
+}
